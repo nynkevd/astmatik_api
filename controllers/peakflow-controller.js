6a853b4c14fce1b0f694699555c60febe518bb66
nynkevd <37672196+nynkevd@users.noreply.github.com>
2020-12-18 17:11:08 +0100

    Fixed peakflow graph

--- controllers/peakflow-controller.js
+++ controllers/peakflow-controller.js
@@ -24,9 +24,9 @@ const getOverview = async (req, res) => {
     }
 
     let overview = {today: [], thisWeek: {
-        beforeMedication: [], afterMedication: []
+        beforeMedication: {morning: [], evening: []}, afterMedication: {morning: [], evening: []}
     }, thisMonth: {
-        beforeMedication: [], afterMedication: []
+        beforeMedication: {morning: [], evening: []}, afterMedication: {morning: [], evening: []}
     }};
 
     let todaysPeakflow = await Peakflow.find({user: userId, timestamp: {$gt: moment().startOf('day').format(), $lt: moment().endOf('day').format()}}).sort({timestamp: 'asc'});
@@ -41,45 +41,61 @@ const getOverview = async (req, res) => {
     }
 
     let thisWeeksPeakflow = await Peakflow.find({user: userId, timestamp: {$gt: moment().startOf('isoWeek').format(), $lt: moment().endOf('isoWeek').format()}}).sort({timestamp: 'asc'});
+    overview.thisWeek.beforeMedication.morning[6] = 0;
+    overview.thisWeek.beforeMedication.evening[6] = 0;
+    overview.thisWeek.afterMedication.morning[6] = 0;
+    overview.thisWeek.afterMedication.evening[6] = 0;
+
     for (const thisPeakflow of thisWeeksPeakflow) {
+        console.log(thisPeakflow);
         let day = moment(thisPeakflow.timestamp).day();
-        console.log(day);       
-        let beforeMedPeakflow = {
-            value: thisPeakflow.beforeMedication,
-            morning: thisPeakflow.morning
-        };
-        let afterMedPeakflow = {
-            value: thisPeakflow.afterMedication,
-            morning: thisPeakflow.morning
-        };
-        overview.thisWeek.beforeMedication[day] = beforeMedPeakflow;
-        overview.thisWeek.afterMedication[day] = afterMedPeakflow;
+        day -= 1;
+        day == -1 && (day = 6);
+
+        console.log(day);  
+        if (thisPeakflow.morning) {
+            overview.thisWeek.beforeMedication.morning[day] = thisPeakflow.beforeMedication;
+            overview.thisWeek.afterMedication.morning[day] = thisPeakflow.afterMedication;
+        } else {
+            overview.thisWeek.beforeMedication.evening[day] = thisPeakflow.beforeMedication;
+            overview.thisWeek.afterMedication.evening[day] = thisPeakflow.afterMedication;  
+        }     
     }
+    
 
     let thisMonthsPeakflow = await Peakflow.find({user: userId, timestamp: {$gt: moment().startOf('month').format(), $lt: moment().endOf('month').format()}}).sort({timestamp: 'asc'});
+    overview.thisMonth.beforeMedication.morning[moment().endOf("month").format("DD")] = 0;
+    overview.thisMonth.beforeMedication.evening[moment().endOf("month").format("DD")] = 0;
+    overview.thisMonth.afterMedication.morning[moment().endOf("month").format("DD")] = 0;
+    overview.thisMonth.afterMedication.evening[moment().endOf("month").format("DD")] = 0;
+    
     for (const thisPeakflow of thisMonthsPeakflow) {
-        let day = moment(thisPeakflow.timestamp).date();      
-        let beforeMedPeakflow = {
-            value: thisPeakflow.beforeMedication,
-            morning: thisPeakflow.morning
-        };
-        let afterMedPeakflow = {
-            value: thisPeakflow.beforeMedication,
-            morning: thisPeakflow.morning
-        };
-        overview.thisMonth.beforeMedication[day] = beforeMedPeakflow;
-        overview.thisMonth.afterMedication[day] = afterMedPeakflow;
+        let day = moment(thisPeakflow.timestamp).date();
+        day -= 1; 
+
+        if (thisPeakflow.morning) {
+            overview.thisMonth.beforeMedication.morning[day] = thisPeakflow.beforeMedication;
+            overview.thisMonth.afterMedication.morning[day] = thisPeakflow.afterMedication;
+        } else {
+            overview.thisMonth.beforeMedication.evening[day] = thisPeakflow.beforeMedication;
+            overview.thisMonth.afterMedication.evening[day] = thisPeakflow.afterMedication;  
+        }     
     }
 
     // console.log(overview);
-    // console.log("======================================================")
+    // console.log("======================================================");
 
-    overview.thisWeek.beforeMedication = Array.from(overview.thisWeek.beforeMedication, item => item || {value: 0});
-    overview.thisWeek.afterMedication = Array.from(overview.thisWeek.afterMedication, item => item || {value: 0});
-    overview.thisMonth.beforeMedication = Array.from(overview.thisMonth.beforeMedication, item => item || {value: 0});
-    overview.thisMonth.afterMedication = Array.from(overview.thisMonth.afterMedication, item => item || {value: 0});
+    overview.thisWeek.beforeMedication.morning = Array.from(overview.thisWeek.beforeMedication.morning, item => item || 0);
+    overview.thisWeek.beforeMedication.evening = Array.from(overview.thisWeek.beforeMedication.evening, item => item || 0);
+    overview.thisWeek.afterMedication.morning = Array.from(overview.thisWeek.afterMedication.morning, item => item || 0);
+    overview.thisWeek.afterMedication.evening = Array.from(overview.thisWeek.afterMedication.evening, item => item || 0);
+    
+    overview.thisMonth.beforeMedication.morning = Array.from(overview.thisMonth.beforeMedication.morning, item => item || 0);
+    overview.thisMonth.beforeMedication.evening = Array.from(overview.thisMonth.beforeMedication.evening, item => item || 0);
+    overview.thisMonth.afterMedication.morning = Array.from(overview.thisMonth.afterMedication.morning, item => item || 0);
+    overview.thisMonth.afterMedication.evening = Array.from(overview.thisMonth.afterMedication.evening, item => item || 0);
 
-    // console.log(overview);
+    console.log(overview);
     console.log("sending");
     return res.status(200).json(overview);
 }
